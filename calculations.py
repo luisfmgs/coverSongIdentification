7141bd82f7f6c1e0a3da4736cc96276d0ccf3143
Luís Filipe Soutello <luisfmgs@gmail.com>
2017-10-18 23:58:32 -0200

    hop time changes

--- calculations.py
+++ calculations.py
@@ -4,13 +4,16 @@ def extract_features(song, sample_rate, B=14):
     from numpy import mean
     from numpy.linalg import norm
 
-    # Calculate the number of samples in 23 ms
-    beat_tracking_hop = sample_rate*23/1000
+    # Calculate the number of samples in 25 ms
+    beat_tracking_hop = sample_rate*25/1000
 
     tempo, beat_frames = beat_track(y=song, sr=sample_rate, hop_length=beat_tracking_hop)
 
-    avarage_tempo_length = int(60*sample_rate/tempo)
-    mfcc_hop = avarage_tempo_length/200
+    #avarage_tempo_length = int(60*sample_rate/tempo)
+    #mfcc_hop = avarage_tempo_length/200
+
+    avarage_tempo_length = sample_rate*95/1000
+    mfcc_hop = avarage_tempo_length/4
 
     initial_block_index = 0
     final_block_index = B*avarage_tempo_length
@@ -34,3 +37,17 @@ def self_similarity_matrix(feature_matrix):
     from scipy.spatial.distance import pdist, squareform
 
     return squareform(pdist(feature_matrix))
+
+def cross_similatity_matrix(list_a, list_b):
+    from numpy import zeros
+    from numpy.linalg import norm
+
+    n = len(list_a)
+    m = len(list_b)
+
+    csm = zeros((n,m))
+
+    for i in range(n):
+        for j in range(m):
+            csm[i][j] = norm(list_a[i] - list_b[j])
+
--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from librosa import load as loadSong
 from json import load as jsonLoad
 import calculations
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 N = 1
 
 
@@ -11,9 +11,9 @@ with open(songsInfoPath) as data:
     songsInfo = jsonLoad(data)
 
 originalsong, sr1 = loadSong(path=songsInfo[N]['originalPath'], sr=None)
-#vocalcover, sr2 = loadSong(path=songsInfo[N]['vocalCoverPath'], sr=None)
+vocalcover, sr2 = loadSong(path=songsInfo[N]['vocalCoverPath'], sr=None)
 #instrumentalcover, sr3 = loadSong(path=songsInfo[N]['instrumentalCoverPath'], sr=None)
 
 originalfeatures = calculations.extract_features(originalsong, sr1)
-#vocalfeatures = calculations.extract_features(vocalcover, sr2)
+vocalfeatures = calculations.extract_features(vocalcover, sr2)
 #instrumentalfeatures = calculations.extract_features(instrumentalcover, sr3)
