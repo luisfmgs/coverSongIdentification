normalization fix

feature_extraction.py
@@ -1,5 +1,5 @@
-def extract_features(song,sr):
-    return ssm(song,sr)
+def extract_features(song, sr):
+    return ssm(song, sr)
 
 
 def beat_synchronous_chroma(song, sr):
@@ -15,7 +15,8 @@ def beat_synchronous_chroma(song, sr):
 
     return sync(chromagram, beat_frames)
 
-def beat_synchronous_mfcc(song,sr):
+
+def beat_synchronous_mfcc(song, sr):
     from librosa.beat import beat_track
     from librosa.feature import mfcc
     from librosa.util import sync
@@ -28,21 +29,21 @@ def beat_synchronous_mfcc(song,sr):
 
     return sync(mfccs, beat_frames)
 
-def both_beat_synchronous(song,sr):
+
+def both_beat_synchronous(song, sr):
     from numpy import vstack
     beat_chroma = beat_synchronous_chroma(song,sr)
     beat_mfcc = beat_synchronous_mfcc(song,sr)
 
     return vstack([beat_chroma, beat_mfcc])
 
-def ssm(song,sr,n_beats=9):
-    from scipy.spatial.distance import pdist, squareform
-    from numpy import asarray, mean
-    from numpy.linalg import norm
 
-    mfccs = beat_synchronous_mfcc(song,sr).transpose()
-    for i in range(mfccs.shape[0]):
-        mfccs[i] = (mfccs[i] - mean(mfccs[i])) / norm(mfccs[i] - mean(mfccs[i]))
+def ssm(song, sr, n_beats=12):
+    from scipy.spatial.distance import pdist, squareform
+    from numpy import asarray
+
+    mfccs = beat_synchronous_chroma(song,sr).transpose()
+    mfccs = normalize(mfccs)
 
     initial_block_index = 0
     final_block_index = n_beats
@@ -54,6 +55,17 @@ def ssm(song,sr,n_beats=9):
         final_block_index += 1
 
     return asarray(result)
+
+
+def normalize(matrix):
+    from numpy import mean
+    from numpy.linalg import norm
+    n = matrix.shape[0]
+    for i in range(n):
+        if norm(matrix[i] - mean(matrix[i]) != 0):
+            matrix[i] = (matrix[i] - mean(matrix[i]))/norm(matrix[i] - mean(matrix[i]))
+
+    return matrix
 
 
 